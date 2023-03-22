041eae4c11264c46cb2064fe58852a80e830c5ee
pwtansg <pwtan.edu@gmail.com>
2023-03-23 00:49:50 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,16 +1,10 @@
 <?php 
 
 //DB constants
-// define("DB_HOST", "localhost");
-// define("DB_USER", "root");
-// define("DB_PASS", "");
-// define("DB_NAME", "job_db");
-
-//heroku constants
-define("DB_HOST", "us-cdbr-east-06.cleardb.net");
-define("DB_USER", "b9283a16649491");
-define("DB_PASS", "a7ab65e2");
-define("DB_NAME", "heroku_20b500b94556ee1");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "job_db");
 
 define("CO_NAME", "Job Listings");
 define("SITE_TITLE", "Job Listings");
